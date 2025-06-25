5759aca0954d7502c8d2ba6042857a54e41fd669
MateusLima <165662012+GoatMateusLima@users.noreply.github.com>
2025-06-24 23:45:13 -0300

    troquei a porta do app.py e criei uma linha de decisao caso o render tenha ou nao uma porta

--- projeto_bonate/Flask/app.py
+++ projeto_bonate/Flask/app.py
@@ -102,4 +102,5 @@ def atualizar_quantidade():
 
 # Executar
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Pega a porta do Render, se tiver, senão usa 5000 localmente
+    app.run(debug=True, host='0.0.0.0', port=port)
